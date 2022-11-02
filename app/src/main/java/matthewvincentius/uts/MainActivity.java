1804a23a9c8ff152aadd8af999ed143686ee5867
adsa <matthewvincentius01@gmail.com>
2022-11-02 13:04:37 +0700

    UTS

--- app/src/main/java/matthewvincentius/uts/MainActivity.java
+++ app/src/main/java/matthewvincentius/uts/MainActivity.java
@@ -2,11 +2,13 @@ package matthewvincentius.uts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 private EditText et_nama, et_nomor;
@@ -29,7 +31,45 @@ private Button btn_daftar;
             @Override
             public void onClick(View view) {
                 String nama, nomor;
+                String facebook, instagram, website;
+                int selectedID;
 
+                nama = et_nama.getText().toString();
+                nomor = et_nomor.getText().toString();
+
+                if (nama.trim().equals("")){
+                    et_nama.setError("Wajib Isi Nama!");
+                }
+                else if (nomor.trim().equals("")){
+                    et_nomor.setError(("Nomor Pendaftaran Wajib Diisi!"));
+                }
+                else if (!cb_fb.isChecked()&&!cb_ig.isChecked()&&!cb_web.isChecked()){
+                    Toast.makeText(getApplicationContext(), "Pilih Salah Satu Sumber Informasi!", Toast.LENGTH_SHORT).show();
+                }
+                else if (cb_fb.isChecked())
+                Toast.makeText(getApplicationContext(), "Facebook", Toast.LENGTH_SHORT).show();
+
+                else if (cb_ig.isChecked())
+                    Toast.makeText(getApplicationContext(), "Instagram", Toast.LENGTH_SHORT).show();
+
+                else if (cb_web.isChecked())
+                    Toast.makeText(getApplicationContext(), "Website", Toast.LENGTH_SHORT).show();
+                else{
+                    Intent intent = new Intent(MainActivity.this, UTS.class);
+                    intent.putExtra("varNama", nama);
+                    intent.putExtra("varNomor", nomor);
+                    startActivity(intent);
+                }
+                switch (view.getId()){
+                    case R.id.cb_fb:
+
+                        break;
+                    case R.id.cb_ig:
+
+                    case R.id.cb_web:
+
+
+                }
             }
         });
 
--- app/src/main/java/matthewvincentius/uts/UTS.java
+++ app/src/main/java/matthewvincentius/uts/UTS.java
@@ -2,14 +2,32 @@ package matthewvincentius.uts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class UTS extends AppCompatActivity {
-
+    private TextView tv_nama, tv_nomor;
+    private CheckBox cb_fb, cb_ig, cb_web;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uts);
+
+        tv_nama = findViewById(R.id.tv_nama);
+        tv_nomor = findViewById(R.id.tv_nomor);
+
+        cb_fb = findViewById(R.id.cb_fb);
+        cb_ig = findViewById(R.id.cb_ig);
+        cb_web = findViewById(R.id.cb_web);
+
+        Intent intent = getIntent();
+        tv_nama.setText(intent.getStringExtra("varNama"));
+        tv_nomor.setText(intent.getStringExtra("varNomor"));
+        cb_ig.setText(intent.getStringExtra("varIg"));
+        cb_fb.setText(intent.getStringExtra("varFb"));
+        cb_web.setText(intent.getStringExtra("varWeb"));
 
     }
 }
